bdd1b48038c3189ac52653f019660307d4c3dcf1
afimpel <afimpel@mowplayer.com>
2024-02-05 19:04:21 -0300

    MAC ADDR

--- make-libvirt.sh
+++ make-libvirt.sh
@@ -5,12 +5,15 @@ else
   CHANGEram=$(( $1 * 1048576))
 fi
 CHANGEramGB=$(( ( $CHANGEram / 1024 ) / 1024 ));
-echo "RAM: $CHANGEramGB GB";
+macaddr=$(echo $FQDN|md5sum|sed 's/^\(..\)\(..\)\(..\)\(..\)\(..\).*$/02:\1:\2:\3:\4:\5/')
+CHANGEramGB=$(( ( $CHANGEram / 1024 ) / 1024 ));
+echo "RAM: $CHANGEramGB GB / MAC ADDR: $macaddr / UUID: ${UUID}";
 UUID=$(cat /proc/sys/kernel/random/uuid)
 sed "s/CHANGEME/$USER/g" macOS-libvirt.xml > macOS00.xml
 sed "s/CHANGEramGB/$CHANGEramGB/g" macOS00.xml > macOS01.xml
 sed "s/CHANGEram/$CHANGEram/g" macOS01.xml > macOS02.xml
-sed "s/CHANGEuuid/$UUID/g" macOS02.xml > macOS_${CHANGEramGB}G.xml
+sed "s/CHANGEuuid/$UUID/g" macOS02.xml > macOS03.xml
+sed "s/changeMacADDR/$macaddr/g" macOS03.xml > macOS_${UUID}.xml
 virt-xml-validate macOS_${CHANGEramGB}G.xml
 sudo setfacl -m u:libvirt-qemu:rx /home/$USER
 sudo setfacl -R -m u:libvirt-qemu:rx /home/$USER/OSX-KVM
